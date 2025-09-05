400253efa1b8474fd861c624d306dfde9cf8dc4d
charlie3965 <charlie39653965@gmail.com>
2025-09-05 21:51:44 +0900

    CORS 추가

--- app/main.py
+++ app/main.py
@@ -2,8 +2,28 @@ from fastapi import FastAPI, Depends
 from app.utils.auth import login, auth_callback
 from app.utils.db import get_db
 from app.utils.models import User
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EXTENSION_ID=os.getenv("EXTENSION_ID")
 
 app = FastAPI()
+
+# 크롬 익스텐션의 ID를 아래처럼 실제 확장ID로 지정합니다.
+origins = [
+    f"chrome-extension://{EXTENSION_ID}"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 연결
 app.add_api_route("/login", login, methods=["GET"])
